e67c38c4ce0998a412999a4caf19363864ee1771
taikulawo <qaq1362211689@gmail.com>
2024-07-03 14:24:02 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
-use std::io::Cursor;
 use std::net::SocketAddr;
-use std::pin::Pin;
-use std::sync::Arc;
 use std::{env, io};
 
-use anyhow::{bail, Context};
+use anyhow::bail;
 use http::{Request, Response, StatusCode};
 use http_body_util::Full;
 use hyper::body::{Bytes, Incoming};
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper_util::rt::TokioIo;
-use openssl::pkey::PKey;
-use openssl::ssl::{
-    Ssl, SslContext, SslContextBuilder, SslMethod, SslSessionCacheMode, SslVersion,
-};
-use openssl::x509::X509;
-use pki_types::{CertificateDer, PrivateKeyDer};
-use rustls::server::NoServerSessionStorage;
-use rustls::ServerConfig;
 use socket2::{Domain, Type};
 use tokio::net::TcpListener;
-use tokio_rustls::TlsAcceptor;
 #[cfg(all(feature = "ossl", not(feature = "rustls")))]
 mod ossl;
+#[cfg(feature = "rustls")]
+mod rustls;
 #[cfg(all(not(target_env = "msvc"), feature = "jemallocator"))]
 use jemallocator::Jemalloc;
 
@@ -33,9 +20,6 @@ static GLOBAL: Jemalloc = Jemalloc;
 
 static CERT: &[u8] = include_bytes!("../certs/www.example.org.full.cert.pem");
 static KEY: &[u8] = include_bytes!("../certs/www.example.org.full.key.pem");
-fn error(err: String) -> io::Error {
-    io::Error::new(io::ErrorKind::Other, err)
-}
 
 fn create_listener_socket(port: usize) -> anyhow::Result<TcpListener> {
     let socket = socket2::Socket::new(Domain::IPV6, Type::STREAM, None)?;
@@ -60,7 +44,10 @@ fn spawn_local(port: usize) {
         .block_on(async move {
             let listener = create_listener_socket(port).unwrap();
             #[cfg(feature = "rustls")]
-            run_server(listener).await.unwrap();
+            {
+                use rustls::run_server;
+                run_server(listener).await.unwrap();
+            }
             #[cfg(all(feature = "ossl", not(feature = "rustls")))]
             {
                 use ossl::run_openssl_server;
@@ -135,69 +122,6 @@ fn make_daemon() -> anyhow::Result<()> {
         libc::umask(0);
     }
     return Ok(());
-}
-#[cfg(feature = "rustls")]
-async fn run_server(incoming: TcpListener) -> anyhow::Result<()> {
-    use rustls::ALL_VERSIONS;
-
-    #[cfg(feature = "aws-lc-rs")]
-    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
-    // set file limits
-    let li = libc::rlimit {
-        rlim_cur: 1000000,
-        rlim_max: 1000000,
-    };
-    unsafe {
-        if libc::setrlimit(libc::RLIMIT_NOFILE, &li) == -1 {
-            panic!("setrlimit error")
-        }
-    };
-
-    fn load_certs() -> io::Result<Vec<CertificateDer<'static>>> {
-        let mut reader = io::BufReader::new(Cursor::new(CERT));
-        rustls_pemfile::certs(&mut reader).collect()
-    }
-
-    fn load_private_key() -> io::Result<PrivateKeyDer<'static>> {
-        let mut reader = io::BufReader::new(Cursor::new(KEY));
-        rustls_pemfile::private_key(&mut reader).map(|key| key.unwrap())
-    }
-
-    let certs = load_certs()?;
-    let key = load_private_key()?;
-
-    println!("serve tls server on {}", incoming.local_addr().unwrap());
-
-    let mut server_config = ServerConfig::builder_with_protocol_versions(ALL_VERSIONS)
-        .with_no_client_auth()
-        .with_single_cert(certs, key)
-        .map_err(|e| error(e.to_string()))?;
-    // disable tls session cache
-    server_config.session_storage = Arc::new(NoServerSessionStorage {});
-    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
-    let service = service_fn(echo);
-    loop {
-        let (tcp_stream, _remote_addr) = match incoming.accept().await {
-            Ok(x) => x,
-            Err(err) => {
-                eprintln!("tcp accept error: {err:#}");
-                continue;
-            }
-        };
-        let tls_acceptor = tls_acceptor.clone();
-        tokio::spawn(async move {
-            let s = match tls_acceptor.accept(tcp_stream).await {
-                Ok(tls_stream) => tls_stream,
-                Err(err) => {
-                    eprintln!("failed to perform tls handshake: {err:#}");
-                    return;
-                }
-            };
-            let mut h1 = http1::Builder::new();
-            h1.keep_alive(false);
-            let _ = h1.serve_connection(TokioIo::new(s), service).await;
-        });
-    }
 }
 pub const DEFAULT_CIPHER: &str = "HIGH:!aNULL:!MD5";
 pub const DEFAULT_TLS12_CIPHER: &str = DEFAULT_CIPHER;
---
+++ src/rustls.rs
@@ -0,0 +1,76 @@
+use std::{
+    io::{self, Cursor},
+    sync::Arc,
+};
+
+use crate::{echo, CERT, KEY};
+use hyper::{server::conn::http1, service::service_fn};
+use hyper_util::rt::TokioIo;
+use pki_types::{CertificateDer, PrivateKeyDer};
+use rustls::{server::NoServerSessionStorage, ServerConfig, ALL_VERSIONS};
+use tokio::net::TcpListener;
+use tokio_rustls::TlsAcceptor;
+
+fn error(err: String) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, err)
+}
+pub(super) async fn run_server(incoming: TcpListener) -> anyhow::Result<()> {
+    #[cfg(feature = "aws-lc-rs")]
+    let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+    // set file limits
+    let li = libc::rlimit {
+        rlim_cur: 1000000,
+        rlim_max: 1000000,
+    };
+    unsafe {
+        if libc::setrlimit(libc::RLIMIT_NOFILE, &li) == -1 {
+            panic!("setrlimit error")
+        }
+    };
+
+    fn load_certs() -> io::Result<Vec<CertificateDer<'static>>> {
+        let mut reader = io::BufReader::new(Cursor::new(CERT));
+        rustls_pemfile::certs(&mut reader).collect()
+    }
+
+    fn load_private_key() -> io::Result<PrivateKeyDer<'static>> {
+        let mut reader = io::BufReader::new(Cursor::new(KEY));
+        rustls_pemfile::private_key(&mut reader).map(|key| key.unwrap())
+    }
+
+    let certs = load_certs()?;
+    let key = load_private_key()?;
+
+    println!("serve tls server on {}", incoming.local_addr().unwrap());
+
+    let mut server_config = ServerConfig::builder_with_protocol_versions(ALL_VERSIONS)
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|e| error(e.to_string()))?;
+    // disable tls session cache
+    server_config.session_storage = Arc::new(NoServerSessionStorage {});
+    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
+    let service = service_fn(echo);
+    loop {
+        let (tcp_stream, _remote_addr) = match incoming.accept().await {
+            Ok(x) => x,
+            Err(err) => {
+                eprintln!("tcp accept error: {err:#}");
+                continue;
+            }
+        };
+        let tls_acceptor = tls_acceptor.clone();
+        tokio::spawn(async move {
+            let s = match tls_acceptor.accept(tcp_stream).await {
+                Ok(tls_stream) => tls_stream,
+                Err(err) => {
+                    eprintln!("failed to perform tls handshake: {err:#}");
+                    return;
+                }
+            };
+            let mut h1 = http1::Builder::new();
+            h1.keep_alive(false);
+            let _ = h1.serve_connection(TokioIo::new(s), service).await;
+        });
+    }
+}
